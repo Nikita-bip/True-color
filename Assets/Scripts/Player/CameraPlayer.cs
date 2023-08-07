d60f2878448d68bd391d272fd8c4eb839b36d32c
Леонтьев Никита <78549055+Nikita-bip@users.noreply.github.com>
2023-08-07 13:03:17 +0300

    Update CameraPlayer.cs

--- Assets/Scripts/Player/CameraPlayer.cs
+++ Assets/Scripts/Player/CameraPlayer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class CameraPlayer : MonoBehaviour
 {
     [SerializeField] private List<Player> _players;
-    //[SerializeField] private Transform _player;
     [SerializeField] private float _smoothSpeed = 0.125f;
     [SerializeField] private Vector3 _offset;
 
@@ -21,12 +20,5 @@ public class CameraPlayer : MonoBehaviour
                 transform.LookAt(player.transform);
             }
         }
-
-        //Vector3 desiredPosition = _player.position + _offset;
-        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
-        //transform.position = smoothedPosition;
-
-        //transform.LookAt(_player);
     }
-
 }
