701e8e80aa758453b073cb30ea79580a70a51f6d
Леонтьев Никита <78549055+Nikita-bip@users.noreply.github.com>
2023-08-28 22:44:20 +0300

    clean git

--- Assets/Scripts/UI/Shop/Money.cs
+++ Assets/Scripts/UI/Shop/Money.cs
@@ -9,8 +9,4 @@ public class Money : MonoBehaviour
     {
         _countOfMoney.text = PlayerPrefs.GetInt(Constantes.StrCountMoney).ToString();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 7fcba35c0f20af8c87af357ea4ab3ee4ccb24927
